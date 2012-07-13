200052d1ab5e8566d6b7bdfcf23c01d2cfbf5a5a
Deepwinter <deepwinter@winterroot.net>
2012-07-12 22:43:46 -0400

    Updated to print all of the PHP auto variables recursively

--- restlog.php
+++ restlog.php
@@ -1,9 +1,21 @@
 <?php
+function foreachPrint($fh, $myArray){
+  foreach($myArray as $h=>$v){
+    fwrite($fh, "$h = $v\n");
+    if(is_array($v)){
+      foreachPrint($fh, $v);
+    }
+  }
+}
 $myFile = "requestslog.txt";
 $fh = fopen($myFile, 'a') or die("can't open file");
 fwrite($fh, "\n\n---------------------------------------------------------------\n");
 foreach($_SERVER as $h=>$v)
     fwrite($fh, "$h = $v\n");
+fwrite($fh, "REQUEST\n");
+foreachPrint($fh, $_REQUEST);
+fwrite($fh, "FILES\n");
+foreachPrint($fh, $_FILES);
 fwrite($fh, "\r\n");
 fwrite($fh, file_get_contents('php://input'));
 fclose($fh);
